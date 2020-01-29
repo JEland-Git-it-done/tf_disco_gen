2c2618420740820f2cb7d74849072b2842078b72
jedel <jedeland@live.co.uk>
2020-01-29 14:38:59 +0100

    Initial commit

---
+++ venv/combiner_script.py
@@ -0,0 +1,8 @@
+
+'''
+Recently i was playing the game Disco Elysium (see: https://store.steampowered.com/app/632470/Disco_Elysium/ ) and was looking at the creators FAQ's and inspirations
+A listing noted that the games artwork was heavily inspired by certain creators
+This project aims to create a way for users to upload their own photo and "Disco Elysium-ify it" to maybe
+Use with mods or for personal use
+The following artists where used, Alex Kanevsky, Jenny Savlille, Wassily Kandinsky
+'''
